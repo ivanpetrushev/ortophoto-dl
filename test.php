afb3cc42af27233b055d648b753701156980f96f
Ivan Petrushev <ivan.petrushev@dnhsoft.com>
2018-03-21 11:04:11 +0200

    first commit

---
+++ test.php
@@ -0,0 +1,156 @@
+<?php
+
+//$maps = [
+//    'alyosha' => [
+//        'hash' => '01115c1bd59e4781a123ce2e267a83f3',
+//        'bbox' => [42.147401, 24.734988, 42.140981, 24.744687],
+//        'dir' => '2018_02_01_Alyosha',
+//        'url' => 'https://s3-us-west-1.amazonaws.com/3-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png'
+//    ],
+//    'old_town' => [
+//        'url' => 'https://s3-us-west-1.amazonaws.com/b-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+//        'bbox' => [42.153343, 24.747895, 42.146725, 24.757991],
+//        'dir' => '2017_12_15_Plovdiv_Old_Town',
+//        'hash' => '705e641dc9fb418bb6a747975511bf0b'
+//    ],
+//    'pd_center_2' => [
+//        'url' => 'https://s3-us-west-1.amazonaws.com/0-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+//        'bbox' => [42.152408, 24.741747, 42.146112, 24.7487],
+//        'dir' => '2017_12_14_Plovdiv_Central_2',
+//        'hash' => '2700616d33b5476a9598e0be22fd0e50'
+//    ],
+//    'pd_center_1' => [
+//        'url' => 'https://s3-us-west-1.amazonaws.com/4-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+//        'hash' => '59e95b5fdf6b4d4f9d8decdc6f6f8de4',
+//        'bbox' => [42.153188, 24.744853, 42.146971, 24.752809],
+//        'dir' => '2017_12_14_Plovdiv_Central_1'
+//    ],
+//    'dondukova' => [
+//        'url' => 'https://s3-us-west-1.amazonaws.com/6-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+//        'hash' => '82a1a6609d8c4a229003fbbb647c13b6',
+//        'bbox' => [42.152094, 24.73737, 42.14678, 24.747283],
+//        'dir' => '2017_12_13_Dondukov_Garden'
+//    ],
+//    'marica2' => [
+//        'url' => 'https://s3-us-west-1.amazonaws.com/1-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+//        'hash' => 'c57f1722d5f148f6ad8cd3b9aa2cced1',
+//        'bbox' => [42.156604, 24.74193, 42.1525, 24.749376],
+//        'dir' => '2017_12_12_Marica_2'
+//    ],
+//    'marica1' => [
+//        'url' => 'https://s3-us-west-1.amazonaws.com/8-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+//        'hash' => '481221fafb734fa6b5239665e430c188',
+//        'bbox' => [42.157614, 24.745717, 42.152826, 24.754021],
+//        'dir' => '2017_12_12_Marica_1'
+//    ],
+//    'central_square_fixed' => [
+//        'url' => 'https://s3-us-west-1.amazonaws.com/7-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+//        'hash' => '90c014e198f14d3280754bdc5c887d97',
+//        'bbox' => [42.146235, 24.747139, 42.1397, 24.752798],
+//        'dir' => '2017_12_02_Central_Square_fixed'
+//    ],
+//    'billa' => [
+//        'url' => 'https://s3-us-west-1.amazonaws.com/f-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+//        'hash' => '87744c7a968347c6a4e055172976671f',
+//        'bbox' => [42.15518, 24.759579, 42.151298, 24.768527],
+//        'dir' => '2017_11_26_Billa_Grobishte'
+//    ],
+//    'bazilika1' => [
+//        'url' => 'https://s3-us-west-1.amazonaws.com/5-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+//        'hash' => '782259c60f3140ffad1c6a899764dc75',
+//        'bbox' => [42.148395, 24.749923, 42.142692, 24.752895],
+//        'dir' => '2017_12_02_Bazilika'
+//    ]
+//];
+
+$maps = [
+    'samarsko' => [
+        'url' => 'https://s3-us-west-1.amazonaws.com/8-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+        'hash' => '18bc3c0c08eb483483558567842cd888',
+        'bbox' => [42.437584, 25.649514, 42.429063, 25.663741],
+        'dir' => '2018_02_18_Samarsko_Zname'
+    ],
+    'zelen_klin' => [
+        'url' => 'https://s3-us-west-1.amazonaws.com/1-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+        'hash' => 'd79082655ada497f9753ad5afed4d991',
+        'bbox' => [42.43326, 25.632219, 42.430101, 25.640899],
+        'dir' => '2018_01_20_ZelenKlin'
+    ],
+    'stancionna' => [
+        'url' => 'https://s3-us-west-1.amazonaws.com/a-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+        'hash' => '3e2221fd479c470da42084f1ea234b4a',
+        'bbox' => [42.420922, 25.625471, 42.415861, 25.631275],
+        'dir' => '2018_01_20_Stancionna'
+    ],
+    'piperka' => [
+        'url' => 'https://s3-us-west-1.amazonaws.com/1-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+        'hash' => 'd428ba4c1c714630ae4585fc88c9cf31',
+        'bbox' => [42.429091, 25.633292, 42.427891, 25.638619],
+        'dir' => '2018_01_20_Piperka'
+    ],
+    'artileriiski' => [
+        'url' => 'https://s3-us-west-1.amazonaws.com/6-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+        'hash' => '28875493f25d43fbb2aff0d0c9c22186',
+        'bbox' => [42.421445, 25.606878, 42.416487, 25.616566],
+        'dir' => '2018_01_14_Artileriiski'
+    ],
+    'bedechka' => [
+        'url' => 'https://s3-us-west-1.amazonaws.com/f-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+        'hash' => '3aaaab9003df4a47b3cafbae4379043f',
+        'bbox' => [42.442097, 25.63267, 42.436539, 25.643399],
+        'dir' => '2017_12_10_Bedechka'
+    ],
+    'zagorka' => [
+        'url' => 'https://s3-us-west-1.amazonaws.com/c-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+        'hash' => 'e36fc47b730f4154b3da74aae256b19c',
+        'bbox' => [42.447101, 25.630417, 42.447101, 25.630417],
+        'dir' => '2017_12_10_Zagorka'
+    ],
+    'kaufland_1' => [
+        'url' => 'https://s3-us-west-1.amazonaws.com/d-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+        'hash' => 'fdf36759feea43b1b90ab198fb38408d',
+        'bbox' => [42.436539, 25.636339, 42.434044, 25.64519],
+        'dir' => '2017_12_10_Kaufland_1'
+    ],
+    'kaufland_2' => [
+        'url' => 'https://s3-us-west-1.amazonaws.com/1-tiles.mapsmadeeasy.com/_X_/021a523c0e344800b8cb98add0feab64/_HASH_/_Z_/_X_/_Y_.png',
+        'hash' => 'dce205f1db604f0b8bae314fd88f6081',
+        'bbox' => [42.433918, 25.636328, 42.431605, 25.645062],
+        'dir' => '2017_12_10_Kaufland_2'
+    ]
+];
+
+
+foreach ($maps as $map => $details) {
+    @mkdir($details['dir']);
+    for ($z = 13; $z <= 20; $z++) {
+        @mkdir($details['dir'] . '/' . $z);
+        $nw = llz2tile($details['bbox'][0], $details['bbox'][1], $z);
+        $se = llz2tile($details['bbox'][2], $details['bbox'][3], $z);
+        for ($x = $nw['xtile']; $x <= $se['xtile']; $x++) {
+            @mkdir($details['dir'] . '/' . $z . '/' . $x);
+            for ($y = $se['ytile']; $y <= $nw['ytile']; $y++) {
+                $zoomurl = str_replace('_Z_', $z, $details['url']);
+                $zoomurl = str_replace('_X_', $x, $zoomurl);
+                $zoomurl = str_replace('_Y_', $y, $zoomurl);
+                $zoomurl = str_replace('_HASH_', $details['hash'], $zoomurl);
+                print "URL: $zoomurl\n";
+                file_put_contents($details['dir'] . '/' . $z . '/' . $x . '/' . $y . '.png', file_get_contents($zoomurl));
+            }
+        }
+    }
+}
+
+
+function llz2tile($lat, $lon, $zoom)
+{
+    $xtile = floor((($lon + 180) / 360) * pow(2, $zoom));
+    $ytile = floor((1 - log(tan(deg2rad($lat)) + 1 / cos(deg2rad($lat))) / pi()) /2 * pow(2, $zoom));
+
+    $ymax = 1 << $zoom;
+    $ytile = $ymax - $ytile - 1;
+
+//    print "XTILE: $xtile YTILE: $ytile\n";
+    return ['xtile' => $xtile, 'ytile' => $ytile];
+}
+
